87d28e8552ddc94414ff9227d6ef3216ed783ac1
saito-ren <renxile0330@gmail.com>
2020-09-24 13:10:41 +0000

    capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,6 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
+server '54.150.150.162', user: 'ec2-user', roles: %w{app web db}
 
 
 
@@ -59,3 +60,5 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+
+
